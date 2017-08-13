23df3c4455ba71a92c00b6f5f108821ac92e8df0
s3uman <nagasumanth48@gmail.com>
2017-08-13 12:43:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function createTemplate(data)
     var heading=data.heading;
     var date=data.date;
     var content=data.content;
-    var htmltemplate=
+    var htmlTemplate=
     `<html>
     <head>
     <title>
