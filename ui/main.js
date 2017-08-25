b157cdc26cf58aef87faf2fad71610bc05cd8428
s3uman <nagasumanth48@gmail.com>
2017-08-25 22:02:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New-value';
 var element=document.getElementById('madi');
-madi.onclick= function ()
+img.onclick= function ()
 {
-    madi.style.marginleft= "200px";
+    img.style.marginleft= "200px";
     
 };
