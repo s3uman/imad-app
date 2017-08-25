0b06eeea110ae2fa99a4abcd791bf358eba9c40b
s3uman <nagasumanth48@gmail.com>
2017-08-25 14:07:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,10 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
