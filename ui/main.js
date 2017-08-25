9a550e5d40c80dc0c9d14dc2f63801cd129667ed
s3uman <nagasumanth48@gmail.com>
2017-08-25 22:29:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
+    madi.style.marginLeft=marginLeft+'px';
     
     
 }
