694728666ba7192136f688e604b4dee12de0230e
s3uman <nagasumanth48@gmail.com>
2017-08-26 06:53:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,6 +65,11 @@ app.get('/article-two', function(req,res){
 app.get('/article-three', function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
 
 
 app.get('/ui/style.css', function (req, res) {
