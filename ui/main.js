66970263a664750acca0178722f9e2e801ac8bcc
s3uman <nagasumanth48@gmail.com>
2017-08-26 13:16:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,10 +10,11 @@ button.onclick = function(){
               var counter= request.responseText;
               var span =document.getElementById('count');
               span.innerHTML=counter.toString();
-              counter=counter+1;
+             
           }
       }
     };
+     counter=counter+1;
     request.open('GET','http://nagasumanth48.imad.hasura-app.io/counter',true);
     request.send(null);
     
